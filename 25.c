d5fd5660073025761b61841e83da443c637cc82d
utkarshkharkwal32-cell <utkarshkharkwal32@gmail.com>
2026-02-25 12:15:15 +0530

    Add files via upload

---
+++ 25.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, key, count = 0;
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    // Input number of nodes
+    scanf("%d", &n);
+
+    // Create linked list
+    for (int i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+        } else {
+            temp->next = newNode;
+        }
+        temp = newNode;
+    }
+
+    // Input key element
+    scanf("%d", &key);
+
+    // Traverse and count occurrences
+    temp = head;
+    while (temp != NULL) {
+        if (temp->data == key) {
+            count++;
+        }
+        temp = temp->next;
+    }
+
+    // Output result
+    printf("%d", count);
+
+    return 0;
+}
