4f6a6d681f77ceae0386cf8dcf8bc5960e975cd6
utkarshkharkwal32-cell <utkarshkharkwal32@gmail.com>
2026-03-09 20:24:46 +0530

    Add files via upload

---
+++ 33.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <ctype.h>
+
+char stack[100];
+int top = -1;
+
+void push(char x)
+{
+    stack[++top] = x;
+}
+
+char pop()
+{
+    return stack[top--];
+}
+
+int precedence(char x)
+{
+    if(x=='+' || x=='-')
+        return 1;
+    if(x=='*' || x=='/')
+        return 2;
+    return 0;
+}
+
+int main()
+{
+    char infix[100], postfix[100];
+    int i=0, j=0;
+
+    scanf("%s",infix);
+
+    while(infix[i]!='\0')
+    {
+        if(isalnum(infix[i]))
+        {
+            postfix[j++] = infix[i];
+        }
+        else
+        {
+            while(top!=-1 && precedence(stack[top]) >= precedence(infix[i]))
+            {
+                postfix[j++] = pop();
+            }
+            push(infix[i]);
+        }
+        i++;
+    }
+
+    while(top!=-1)
+    {
+        postfix[j++] = pop();
+    }
+
+    postfix[j] = '\0';
+
+    printf("%s",postfix);
+
+    return 0;
+}
