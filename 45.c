ed3e8b12bd3e2bf36100b9cd65f6a076c3d2928e
utkarshkharkwal32-cell <utkarshkharkwal32@gmail.com>
2026-03-20 23:01:38 +0530

    Add files via upload

---
+++ 45.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Queue for tree construction
+struct Queue {
+    int front, rear;
+    struct Node* arr[100];
+};
+
+void initQueue(struct Queue* q) {
+    q->front = q->rear = 0;
+}
+
+void enqueue(struct Queue* q, struct Node* node) {
+    q->arr[q->rear++] = node;
+}
+
+struct Node* dequeue(struct Queue* q) {
+    return q->arr[q->front++];
+}
+
+// Build tree from level order
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct Queue q;
+    initQueue(&q);
+
+    struct Node* root = newNode(arr[0]);
+    enqueue(&q, root);
+
+    int i = 1;
+
+    while (i < n) {
+        struct Node* curr = dequeue(&q);
+
+        // Left child
+        if (i < n && arr[i] != -1) {
+            curr->left = newNode(arr[i]);
+            enqueue(&q, curr->left);
+        }
+        i++;
+
+        // Right child
+        if (i < n && arr[i] != -1) {
+            curr->right = newNode(arr[i]);
+            enqueue(&q, curr->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// Function to find height
+int height(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    int left = height(root->left);
+    int right = height(root->right);
+
+    return (left > right ? left : right) + 1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct Node* root = buildTree(arr, n);
+
+    printf("%d\n", height(root));
+
+    return 0;
+}
