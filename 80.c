4ed2af7a07fb1c0edbf834ee809ca654bb2a6257
utkarshkharkwal32-cell <utkarshkharkwal32@gmail.com>
2026-04-21 23:16:07 +0530

    Create 80.c

---
+++ 80.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define INF 1000000000  // A large value representing infinity
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int dist[n][n];
+
+    // Input
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &dist[i][j]);
+
+            if(dist[i][j] == -1 && i != j) {
+                dist[i][j] = INF;
+            }
+        }
+    }
+
+    // Floyd-Warshall Algorithm
+    for(int k = 0; k < n; k++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // Output
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(dist[i][j] == INF)
+                printf("-1 ");
+            else
+                printf("%d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
