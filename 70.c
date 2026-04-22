c2b93427ec3d5d79fb8a2ac53857cf0f81b1b82b
utkarshkharkwal32-cell <utkarshkharkwal32@gmail.com>
2026-04-22 23:12:38 +0530

    Create 70.c

---
+++ 70.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <limits.h>
+
+struct Edge {
+    int u, v, w;
+};
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    struct Edge edges[m];
+
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
+    }
+
+    int source;
+    scanf("%d", &source);
+
+    int dist[n + 1];
+
+    // Initialize distances
+    for (int i = 1; i <= n; i++) {
+        dist[i] = INT_MAX;
+    }
+    dist[source] = 0;
+
+    // Relax edges (n-1) times
+    for (int i = 1; i <= n - 1; i++) {
+        for (int j = 0; j < m; j++) {
+            int u = edges[j].u;
+            int v = edges[j].v;
+            int w = edges[j].w;
+
+            if (dist[u] != INT_MAX && dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+            }
+        }
+    }
+
+    // Check for negative weight cycle
+    for (int j = 0; j < m; j++) {
+        int u = edges[j].u;
+        int v = edges[j].v;
+        int w = edges[j].w;
+
+        if (dist[u] != INT_MAX && dist[u] + w < dist[v]) {
+            printf("NEGATIVE CYCLE\n");
+            return 0;
+        }
+    }
+
+    // Print distances
+    for (int i = 1; i <= n; i++) {
+        if (dist[i] == INT_MAX)
+            printf("INF ");
+        else
+            printf("%d ", dist[i]);
+    }
+
+    return 0;
+}
